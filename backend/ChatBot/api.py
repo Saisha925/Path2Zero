8f265b4931be7f93d0375638996345820b4007fa
Lavanya2166 <manochalavanya@gmail.com>
2026-02-08 04:58:01 +0530

    Update api.py

--- backend/ChatBot/api.py
+++ backend/ChatBot/api.py
@@ -225,7 +225,7 @@ def get_footprint(user_id: str):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 
 
